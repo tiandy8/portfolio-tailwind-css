982039bdd76eea88bd7d847651840bdf5f20ea4d
tiandy8 <christiandynathannael.rpla@gmail.com>
2023-06-23 18:30:32 +0700

    optimization

--- dist/js/script.js
+++ dist/js/script.js
@@ -3,10 +3,15 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.querySelector("#to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
+    toTop.classList.add("flex");
   } else {
+    toTop.classList.add("hidden");
+    toTop.classList.remove("flex");
     header.classList.remove("navbar-fixed");
   }
 };
@@ -19,4 +24,13 @@ const navMenu = document.querySelector("#nav-menu");
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("hamburger-active");
   navMenu.classList.toggle("hidden");
+});
+
+// click luar hamburger
+
+window.addEventListener("click", function () {
+  if (e.target != hamburger && e.target != navMenu) {
+    hamburger.classList.remove("hamburger-active");
+    navMenu.classList.add("hidden");
+  }
 });
